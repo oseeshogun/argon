ee99f844455d754cee188e90457cf17d78a07dd6
oseeshogun <omasuaku@gmail.com>
2020-09-30 22:45:10 +0100

    Updates

---
+++ precache-manifest.90017073a48c44c74410be2e39221bfc.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da00d40f3fe12d395ecaa97255f52cf0",
+    "url": "/argon/index.html"
+  },
+  {
+    "revision": "4f968f8ed245b58600bd",
+    "url": "/argon/static/css/main.7a1c7b57.chunk.css"
+  },
+  {
+    "revision": "bdcc3a2dd16486bfa460",
+    "url": "/argon/static/js/2.b420306d.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/argon/static/js/2.b420306d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f968f8ed245b58600bd",
+    "url": "/argon/static/js/main.7352ec02.chunk.js"
+  },
+  {
+    "revision": "cffa2d2d577f49468a08",
+    "url": "/argon/static/js/runtime-main.b3c500cf.js"
+  },
+  {
+    "revision": "275a2629582aa2af3469364c6748cf38",
+    "url": "/argon/static/media/argon-react-white.275a2629.png"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/argon/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/argon/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/argon/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/argon/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/argon/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/argon/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3fc9f001eb2f3cb71fa3cde33086c7db",
+    "url": "/argon/static/media/nqobile-vundla-zOt6a59k2BE-unsplash.3fc9f001.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/argon/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/argon/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/argon/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/argon/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/argon/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "75f7ab18f707114d817522df8dea0a3b",
+    "url": "/argon/static/media/promo-1.75f7ab18.png"
+  },
+  {
+    "revision": "41f8b740a193c87883c9db9731005102",
+    "url": "/argon/static/media/undraw_people_tax5.41f8b740.svg"
+  }
+]);
