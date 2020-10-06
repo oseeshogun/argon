56be43fee52d8c5d6b0dd5b4234c8273cdb2839c
oseeshogun <omasuaku@gmail.com>
2020-10-06 03:06:46 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/argon/precache-manifest.9c5b1f91e9aa3aeb3490c2a9f032964f.js"
+  "/argon/precache-manifest.6235f0a584b4b28524ba378132d21bb6.js"
 );
 
 self.addEventListener('message', (event) => {
